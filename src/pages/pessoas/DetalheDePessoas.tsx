fef2c163f30b4eb0ff172d489081b13bb84d8386
Gabriela Menezes <gabimenezess10@gmail.com>
2024-01-11 13:26:59 -0300

    feat: adicionado funções para os botões da ferramenta de detalhes

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -1,5 +1,81 @@
+import { useNavigate, useParams } from "react-router-dom"
+import { LayoutBaseDePagina } from "../../shared/layouts";
+import { FerramentasDeDetalhe } from "../../shared/components";
+import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
+import { useEffect, useState } from "react";
+import { LinearProgress } from "@mui/material";
+import Swal from "sweetalert2";
+
 export const DetalheDePessoas: React.FC = () => {
+
+    const { id = 'nova' } = useParams<'id'>();
+    const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() =>{
+        if(id !== 'nova') {
+            setIsLoading(true);
+            PessoasService.getById(Number(id))
+            .then((result) => {
+                setIsLoading(false);
+                if(result instanceof Error) {
+                    alert(result.message)
+                    navigate('/pessoas');
+                }else {
+                    console.log(result)
+                }
+            })
+        }
+    }, [id, navigate])
+    const handleSave = () => {
+        console.log('save');
+    }
+
+    const handleDelete = (id:number) => {
+        Swal.fire({
+          title: "Realmente deseja apagar?",
+          text: "Você não poderá desfazer essa ação!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Sim, deletar!"
+        }).then((result) => {
+          if(result instanceof Error) {
+            Swal.fire({
+              icon: "error",
+              title: "Oops...",
+              text: result.message,
+            });
+          }
+          if (result.isConfirmed) {
+            PessoasService.deleteById(id)
+            Swal.fire({
+              title: "Deletado!",
+              text: "O usuário foi deletado.",
+              icon: "success"
+            });
+            navigate('/pessoas')
+          }
+        });
+      }
     return (
-        <>detalhe de pessoas</>
+        <LayoutBaseDePagina tituloDaPagina={id === 'nova' ? 'Nova pessoa' : 'Editar pessoa'} 
+        barraDeFerramentas={
+          <FerramentasDeDetalhe
+            mostrarBotaoApagar={id !== 'nova'}
+            mostrarBotaoNovo={id !== 'nova'}
+
+            aoClicarEmSalvar={handleSave}
+            aoClicarEmApagar={() => handleDelete(Number(id))}
+            aoClicarEmVoltar={() => navigate(-1)}
+            aoClicarEmNovo={() => navigate('/pessoas/detalhe/nova')}
+          />
+        }>
+            {isLoading && (
+                <LinearProgress variant="indeterminate"/>
+            )}
+            detalhe de pessoas {id}
+        </LayoutBaseDePagina>
     )
 }
